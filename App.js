2e4ef471f886f67b9aaabb73969c8eaa7cf86073
x20193939 <x20193939@student.ncirl.ie>
2021-11-28 16:25:11 +0000

    image upload

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, ImageBackground} from 'react-native';
 
 export default function App() {
+    const handlePress = () => console.log("Text pressed");
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Welcome To FunGalaxy
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const styles = StyleSheet.create({
+      container: {
+       flex: 1,
+       backgroundColor: '#86A8e7',
+       justifyContent: "center",
+       alignItems: "center",
+      },
+      FGStacked: {
+        width: 200,
+        height: 300,
+      } 
+    });
+
+  const DisplayAnImage = () => {
+    return (
+      <view style={styles.container}>
+          <Image
+            style={styles.FGStacked}
+            source={require('./FGStacked.png')}
+      />
+      </view>
+    )
+  }
