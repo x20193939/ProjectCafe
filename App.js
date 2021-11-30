4366e49299ee802e1642005eff934689be762606
x20193939 <x20193939@student.ncirl.ie>
2021-11-30 12:53:47 +0000

    init repository

--- App.js
+++ App.js
@@ -1,40 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, ImageBackground} from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, ImageBackground, Platform} from 'react-native';
 
 export default function App() {
-    const handlePress = () => console.log("Text pressed");
-  
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
+      <Text>
         Welcome To FunGalaxy
       </Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/Images/FGStacked.png')} resizeMode="cover"/>
+      {/* <StatusBar style="auto" />  */}
     </SafeAreaView>
   );
 }
-
-    const styles = StyleSheet.create({
-      container: {
-       flex: 1,
-       backgroundColor: '#86A8e7',
-       justifyContent: "center",
-       alignItems: "center",
-      },
-      FGStacked: {
-        width: 200,
-        height: 300,
-      } 
-    });
-
-  const DisplayAnImage = () => {
-    return (
-      <view style={styles.container}>
-          <Image
-            style={styles.FGStacked}
-            source={require('./FGStacked.png')}
-      />
-      </view>
-    )
-  }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#86A8e7',
+    justifyContent: "center",
+    alignItems: "center",
+   
+  },
+  /* Image: {
+    height: '200',
+    width: '300',
+    resizeMode: 'contain',
+  } */
+});
